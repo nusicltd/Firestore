c756591d6191f49be1b64423318af7c9d79afa19
Daniel Asher <daniel.asher.2303@gmail.com>
2020-09-18 15:43:18 +0100

    Update demo/User.kt to include property `ignoredByLackOfPropertyDelegate` (working) and `ignoredByAnnotation` (not working)

--- demo/src/main/kotlin/com/otalistudios/firestore/demo/User.kt
+++ demo/src/main/kotlin/com/otalistudios/firestore/demo/User.kt
@@ -2,16 +2,20 @@
 
 package com.otalistudios.firestore.demo
 
+import com.google.firebase.firestore.Exclude
 import com.otaliastudios.firestore.FirestoreClass
 import com.otaliastudios.firestore.FirestoreDocument
 import com.otaliastudios.firestore.FirestoreList
 import com.otaliastudios.firestore.FirestoreMap
 
-@FirestoreClass
+@FirestoreClass(addUpdatedAt = false)
 class User : FirestoreDocument() {
     var type: Int by this
     var imageUrl: String? by this("image_url")
     var messages: Messages by this
+    var ignoredByLackOfPropertyDelegate: Boolean = true
+    // FIXME: This is currently not working. KotlinPoet issue?
+    @get:Exclude var ignoredByAnnotation: Boolean by this
 
     @FirestoreClass
     class Messages : FirestoreList<Message>()
